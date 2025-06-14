d9c61029a45c74f5623b17807360f8338dc1e84e
Luis Angel Meza Adan <113572028+GenozidAdan@users.noreply.github.com>
2025-06-14 00:36:16 -0600

    puerto cambiado

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,12 +3,12 @@ import cors from 'cors';
 import userRoutes from './routes/userRoutes.js';
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', userRoutes);
+app.use('/', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
